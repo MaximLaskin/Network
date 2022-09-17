4117bcccb9b5df4d1ffc48370adb9b8231ce0505
MaximLaskin <maxlaskin@mail.ru>
2022-09-17 18:03:01 +0300

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by Maxim on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
